259a97da491664323c8ea0a76e6c865adddf4f84
MironMarins <rm551801@fiap.com.br>
2023-09-05 18:33:35 -0300

    realizada imprtações em main.jsx

--- aulas-rotas/src/main.jsx
+++ aulas-rotas/src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import Home from './routes/Home/index'
+import Produtos from './routes/Produtos/index'
+import Error from './routes/Error/index'
+import EditarProdutos from './routes/EditarProdutos/index'
